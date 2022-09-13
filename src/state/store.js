db5a72c606073db038f3b570a2593a01155ced5f
Himanshu <himsingh13@publicisgroupe.net>
2022-09-13 16:49:24 +0530

    change a name of import

--- src/state/store.js
+++ src/state/store.js
@@ -1,5 +1,5 @@
 import { applyMiddleware } from "redux";
-import { createStore } from "redux";
+import { legacy_createStore as createStore } from "redux";
 
 import thunk from "redux-thunk";
 import reducers from "./reducers";
